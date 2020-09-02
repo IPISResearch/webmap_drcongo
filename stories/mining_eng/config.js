f6cdccd54b53fe956a34435ab01f3713da975ad3
Steffest <dev@stef.be>
2020-09-02 10:30:13 +0200

    Added Story

---
+++ stories/mining_eng/config.js
@@ -0,0 +1,109 @@
+var StoryConfig = {
+    title: 'Mapping mining activity in DRC',
+    subtitle: 'An interactive map of mining sites in eastern Democratic Republic of the Congo',
+    byline: '',
+    footer: 'Source: IPIS',
+    alignment: 'left',
+    theme: 'light',
+    chapters: [
+        {
+            id: '1',
+            title: 'Artisanal mining: an important source of revenue...',
+            image: 'walikale.jpg',
+            description: 'Natural resource extraction through artisanal and small scale mining is a major source of revenue in the Democratic Republic of the Congo (DRC). This webmap and accompanying report is based on ten years of research by <a href="https://ipisresearch.be" target="_blank" rel="noopener">IPIS</a> in the eastern parts of the country, focusing on the relationships between mineral resources and conflict. Around 2,700 artisanal mining sites were mapped, with these employing at least 382,000 miners.',
+            urlHash: '#-1.9150376664436521/27.4484335830017/6.162601310261952/4/1/',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            },
+        },
+        {
+            id: '2',
+            title: 'Types of mineral',
+            image: 'chinjira_mine.jpg',
+            description: 'Various materials are mined in DRC, including gold, tin and coltan. Many are integral for production of modern electronics and vehicles so the mines are the first links in lucrative global supply chains. Few stakeholders engaged in artisanal mining have the correct tools for measuring mineral quality, which is an issue as this factor has a significant impact on price.',
+            urlHash: '#-1.685984155536005/28.516341324948826/8.06686610029433/4/1/',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            },
+        },
+        {
+            id: '3',
+            title: 'Destinations',
+            image: 'workers2.jpg',
+            description: 'Most mined materials pass through local and regional trading hubs before being exported to neighbouring countries. Goma and Bukavu are both important regional hubs for consolidating minerals from different mines. Domestic movements to these hubs can be costly: along with transport costs, traders are frequently taxed or forced to pay fees at roadblocks. Mines typically charge lower prices for their products if they are harder for transportation services to access.',
+            urlHash: '#-1.685984155536005/28.516341324948826/8.06686610029433/4/1,6/',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            },
+        },
+        {
+            id: '4',
+            title: 'Interference',
+            image: 'interference.jpg',
+            description: 'Interference refers to external actors making illegal profits from mining or associated trade through extortion, taxation or more indirect methods. Of the province surveyed, South Kivu has the highest proportion of mines affected by interference from armed actors, mostly by units of the Congolese army (FARDC). Direct control of mines by armed actors has diminished in recent years so armed interference now often takes the form of ‘protection rackets’. Non-armed interference also continues to be an issue and is usually perpetrated by unarmed state officials through illegal taxation.',
+            urlHash: '#-3.3371160217914877/28.23735102772025/7.730964113285928/4/1,8/',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            },
+        },
+        {
+            id: '5',
+            title: 'Roadblocks',
+            image: 'roadblock.jpg',
+            description: 'Roadblocks are used to levy illegal taxes on the transport of minerals and are an important illustration of the militarisation of the supply chain. Research by <a href="https://ipisresearch.be/publication/everything-moves-will-taxed-political-economy-roadblocks-north-south-kivu/" target="_blank" rel="noopener">IPIS and the Danish Institute for International Studies</a> in December 2017 identified 798 roadblocks in North and South Kivu, around 75% of which had armed men present, either from Congo’s security forces or non-state armed groups. Typically, roadblocks are strategically located away from the mining sites on junctions connecting sites with a main road.',
+            urlHash: '#-1.246489546768757/28.371795829270127/11.745051128424935/4/1,3/31.x',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            },
+        },
+        {
+            id: '6',
+            title: 'Responsible sourcing',
+            image: 'katimbatimba.jpg',
+            description: 'Initiatives promoting responsible sourcing of minerals have now been running for over a decade in DRC. Of these, <a href="https://www.itsci.org/" target="_blank" rel="noopener">iTSCi</a> is the most prevalant, implementing a programme that matches the due diligence recommendations of the OECD\'s guidance for responsible company management. <a href="https://ipisresearch.be/wp-content/uploads/2019/10/2019-mapping-eastern-DRC-1.pdf" target="_blank" rel="noopener">Comparing mines following due diligence</a> with those that don\'t suggests that the initiatives have had a positive impact, reducing the level of interference by armed actors at mining sites.  Katimbatimba mine, located here, complies with iTSCi and is not currently a victim of armed interference. Notwithstanding the encouraging developments explained above, armed interference in artisanal mining persists, especially in more remote areas.',
+            urlHash: '#-0.9738953799312782/27.70954728126526/14.19760980752313/5/1/4.lc,9.6',
+            location: {
+                pitch: 29.50,
+                bearing: 0.00
+            }
+        },
+               {
+            id: '7',
+            title: 'Social and environmental issues',
+            image: '',
+            description: 'Aside from issues of interference, other responsible sourcing concerns include those relating to the environment and exploitation of vulnerable groups. Some mines use mercury to separate gold from other minerals, releasing hazardous vapour or liquid into the atmosphere, soil and water. This practice is forbidden by DRC\'s mining regulations but is <a href="https://ipisresearch.be/publication/ipis-briefing-april-2020/" target="_blank" rel="noopener">widespread in artisanal gold production</a> because it is cheap, fast and effective. Baru, located here, is one such gold mine that uses mercury treatment and has children in its workforce engaged in activities like washing, crushing, transportation and waste treatment.',
+            urlHash: '#1.962313025983903/30.053400594787718/16.95097507520304/5/1/',
+            location: {
+                pitch: 19.50,
+                bearing: 0.00
+            }
+        },
+        {
+            id: '8',
+            title: 'Earnings',
+            image: 'workers.jpg',
+            description: '73% of DRC\'s population live on less than US$1.90 a day, while miners can make US$2.70 - 3.30 a day. However, in a household with two workers and four or more dependents in eastern DRC, <a href="https://ipisresearch.be/publication/much-miner-earn-assessment-miners-revenue-basic-needs-study-drc/" target="_blank" rel="noopener" >an IPIS research project in 2020</a> focusing specifically on earnings found that mining income still falls short of covering basic needs. In addition, male miners generally earn 29% more than their female counterparts, representing a stark gender imbalance.',
+            urlHash: '#-1.6832832925667702/28.760704228267173/8.706512661381359/4/1/',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            },
+        },
+        {
+            id: '9',
+            title: 'For more information...',
+            image: 'cover.jpg',
+            description: 'IPIS continues to investigate the mining situation in DRC and other countries including the Central African Republic and Tanzania. Head to <a href="https://ipisresearch.be/home/natural-resources/" target="_blank" rel="noopener" >IPIS\' website</a> to find out more about recent and ongoing research projects.',
+            urlHash: '#-1.9150376664436521/27.4484335830017/6.162601310261952/4/1/',
+            location: {
+                pitch: 0.00,
+                bearing: 0.00
+            }
+        },
+    ]
+};
